527d0d38b2f3534ff633aaa97afb8e85c47d2899
jbizima <jbizima@alustudent.com>
2024-06-18 15:06:52 +0200

    push

--- frontend/src/pages/DeleteBook.tsx
+++ frontend/src/pages/DeleteBook.tsx
@@ -56,7 +56,7 @@ const DeleteBook = () => {
       {loading && <Spinner />}
       <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
         <h3 className="text-2xl">Are you sure?</h3>
-        <button className="p-4 bg-red-600" onClick={handleDeleteBook}>
+        <button className="p-4 bg-sky-300 m-8" onClick={handleDeleteBook}>
           Yes
         </button>
       </div>
